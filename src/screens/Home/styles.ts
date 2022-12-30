39657884c7decd40712bf06098e5c82821beb241
Luis Antonio <luis.developer.ac@gmail.com>
2022-12-29 20:51:37 -0500

    stlye(Home): make container as touchable

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,21 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.TouchableOpacity.attrs({
+  activeOpacity: 1,
+})`
   flex: 1;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: #fff;
+`;
+
+export const Wrapper = styled.View`
+  width: 100%;
+  height: 500px;
+  justify-content: space-evenly;
+  flex-direction: row;
+`;
+
+export const Text = styled.Text`
+  font-size: 16px;
 `;
